9308ae8633f91a288f321b704c5b41bf3c54f69e
Nacho Peralta <nachoperalta0@gmail.com>
2022-05-14 15:28:11 -0300

    Environment variables

--- app.js
+++ app.js
@@ -19,8 +19,10 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
+});
 
 const User = new mongoose.model("User", userSchema);
 
